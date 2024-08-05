09b68ba5103ba3e1bc99189e5706106eb9ce8157
길순이 <mmparksb@gmail.com>
2024-08-05 12:30:56 +0900

    박정희의 첫번째 수정

--- src/main/java/com/itgroup/jdbc/InsertMember.java
+++ src/main/java/com/itgroup/jdbc/InsertMember.java
@@ -15,7 +15,7 @@ public class InsertMember {
         String name = scan.next();
 
         // 나 안철수가 먼저 수정할거임
-        // 나 박정희 수정할거임 ㅋㅋ
+        // 나 박정희 수정할거임 ㅋㅋz.. 수정시켜주셈
         Member bean = new Member();
         bean.setId(id);
         bean.setName(name);
